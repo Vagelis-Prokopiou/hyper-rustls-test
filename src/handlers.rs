2f1ac35a685f6f231f47e00b9d8e1ae2c58f630b
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2024-01-15 18:16:49 +0200

    Progress

--- src/handlers.rs
+++ src/handlers.rs
@@ -6,6 +6,7 @@
 //! otherwise HTTP/1.1 will be used.
 
 use crate::middlewares::is_authenticated;
+use crate::middlewares::response_middleware_1;
 use crate::response_bad_request;
 use http::{Method, Request, Response, StatusCode};
 use http_body_util::{BodyExt, Full};
@@ -18,25 +19,39 @@ pub async fn root_handler(
 ) -> Result<Response<Full<Bytes>>, hyper::Error> {
     let mut response = Response::new(Full::default());
 
-    // Request middlewares.
+    // Request middlewares start
     if !is_authenticated(&req) {
         return Ok(response_bad_request(Some("Auth failed")));
     }
+
+    response
+        .headers_mut()
+        .insert("auth", "true".parse().unwrap());
     // Request middlewares end
 
-    match (req.method(), req.uri().path()) {
-        // Help route.
-        (&Method::GET, "/") => {
-            *response.body_mut() = Full::from("Try POST /echo\n");
-        }
-        // Echo service route.
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = Full::from(req.into_body().collect().await?.to_bytes());
-        }
-        // Catch-all 404.
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
+    let response = {
+        match (req.method(), req.uri().path()) {
+            // Help route.
+            (&Method::GET, "/") => {
+                *response.body_mut() = Full::from("Try POST /echo\n");
+                response
+            }
+            // Echo service route.
+            (&Method::POST, "/echo") => {
+                *response.body_mut() = Full::from(req.into_body().collect().await?.to_bytes());
+                response
+            }
+            // Catch-all 404.
+            _ => {
+                *response.status_mut() = StatusCode::NOT_FOUND;
+                response
+            }
         }
     };
+
+    // Request middlewares start
+    let response = response_middleware_1(response);
+    // Request middlewares end
+
     Ok(response)
 }
--- src/middlewares.rs
+++ src/middlewares.rs
@@ -1,5 +1,6 @@
+use http_body_util::Full;
 use hyper::body::Incoming;
-use hyper::Request;
+use hyper::{body::Bytes, Request, Response};
 
 pub fn is_authenticated(req: &Request<Incoming>) -> bool {
     return match req.headers().get("mj_is_goat") {
@@ -9,4 +10,23 @@ pub fn is_authenticated(req: &Request<Incoming>) -> bool {
             Err(_) => false,
         },
     };
+}
+
+pub fn response_middleware_1(response: Response<Full<Bytes>>) -> Response<Full<Bytes>> {
+    let mut r = Response::new(Full::from("aa"));
+    *r.headers_mut() = response.headers().clone();
+    *r.status_mut() = response.status();
+
+    match response.headers().get("auth") {
+        Some(v) => {
+            r.headers_mut()
+                .insert("response_middleware_1", "run".parse().unwrap());
+        }
+        None => {}
+    };
+
+    *r.body_mut() = Full::from(response.into_body());
+
+    //let mut _r = response;
+    return r;
 }
